aa9e5aebe8efff7e67c5d4c246e02907afffeb3f
Audrey Safikhani <safikhani.parasto@gmail.com>
2025-03-05 14:28:37 -0800

    added empty crop file

--- main.py
+++ main.py
@@ -3,27 +3,11 @@ import numpy as np
 import datetime
 from retinaface import RetinaFace
 
-def align(video):  # 5 points, #StyleGAN
-    # Align face to the center of the frame
-    h, w, _ = video[0].shape  # Assuming first frame for alignment
-    center_x, center_y = w // 2, h // 2
-    
-    p1x, p1y, width, height = face_detect(video)
-    if p1x is not None:
-        face_center_x = p1x + width // 2
-        face_center_y = p1y + height // 2
-        
-        dx = center_x - face_center_x
-        dy = center_y - face_center_y
-        
-        translation_matrix = np.float32([[1, 0, dx], [0, 1, dy]])
-        aligned_video = [cv2.warpAffine(frame, translation_matrix, (w, h)) for frame in video]
-        return np.array(aligned_video)
-    
-    return video
-
-def crop(video, point1x, point1y, width, height):  # Padding (ChatGPT) 1000x400
-    return video[:, point1y:point1y + height, point1x:point1x + width]
+def chehre_pre_process(video):
+	align(video)
+	face_detect(video)
+	video = crop(video, )
+       
 
 def face_detect(video):  # Almost done "test.py"
     thresh = 0.8
@@ -52,9 +36,28 @@ def face_detect(video):  # Almost done "test.py"
         box = faces[0].astype(int)  # Taking first detected face
         return box[0], box[1], box[2] - box[0], box[3] - box[1]
     
-    return None, None, None, None
+    return #??
 
-def chehre_pre_process(video):
-	align(video)
-	face_detect(video)
-	video = crop(video, )
+
+def align(video):  # 5 points, #StyleGAN
+    # Align face to the center of the frame
+    h, w, _ = video[0].shape  # Assuming first frame for alignment
+    center_x, center_y = w // 2, h // 2
+    
+    p1x, p1y, width, height = face_detect(video)
+    if p1x is not None:
+        face_center_x = p1x + width // 2
+        face_center_y = p1y + height // 2
+        
+        dx = center_x - face_center_x
+        dy = center_y - face_center_y
+        
+        translation_matrix = np.float32([[1, 0, dx], [0, 1, dy]])
+        aligned_video = [cv2.warpAffine(frame, translation_matrix, (w, h)) for frame in video]
+        return np.array(aligned_video)
+    
+    return video
+
+def crop(video, point1x, point1y, width, height):  # Padding (ChatGPT) 1000x400
+    return video[:, point1y:point1y + height, point1x:point1x + width]
+
